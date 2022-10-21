ab00055a29698c3d20e5c718d0f0b1298eb24675
DiKey10 <kd930603@gmail.com>
2022-10-21 13:28:07 +0200

    init + przed baza

--- blog/routes.py
+++ blog/routes.py
@@ -6,3 +6,28 @@ from blog import app
 @app.route("/")
 def index():
    return render_template("base.html")
+
+"""
+
+from flask import render_template, request
+from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data
+               is_published=form.is_published.data
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+   """
